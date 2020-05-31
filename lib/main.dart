d51c3f1dae94333eab3c0b14a58776afb811b45e
gino abella <gino@ginos-MacBook-Air.local>
2020-05-31 21:35:30 +0800

    last part

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:firebase_login/src/resources/user_repository.dart';
+import 'package:firebase_login/src/authentication_bloc/authentication_bloc.dart';
+import 'package:firebase_login/src/resources/simple_bloc_delegate.dart';
 import 'package:firebase_login/src/app.dart';
 
 void main() {
-  runApp(App());
+  WidgetsFlutterBinding.ensureInitialized();
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  final UserRepository userRepository = UserRepository();
+  runApp(
+    BlocProvider(
+      create: (context) =>
+          AuthenticationBloc(userRepository: userRepository)..add(AppStarted()),
+      child: App(userRepository: userRepository),
+    ),
+  );
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:firebase_login/src/resources/user_repository.dart';
+import 'package:firebase_login/src/authentication_bloc/authentication_bloc.dart';
+import 'package:firebase_login/src/widgets/splash_screen.dart';
+import 'package:firebase_login/src/screens/home_screen.dart';
+import 'package:firebase_login/src/screens/login_screen.dart';
 
 class App extends StatelessWidget {
+  final UserRepository _userRepository;
+
+  App({Key key, @required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository,
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state is Uninitialized) {
+            return SplashScreen();
+          }
+          if (state is Authenticated) {
+            return HomeScreen(name: state.displayName);
+          }
+          if (state is Unauthenticated) {
+            return LoginScreen(userRepository: _userRepository);
+          }
+          return Container();
+        },
+      ),
+    );
   }
 }
